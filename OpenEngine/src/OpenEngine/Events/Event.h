91acc215a58193689d3f2b7019a63e3068d82421
iamk1720@gmail.com <iamk1720@gmail.com>
2023-03-08 21:00:01 +0000

    Refactored Input

--- OpenEngine/src/OpenEngine/Events/Event.h
+++ OpenEngine/src/OpenEngine/Events/Event.h
@@ -33,6 +33,8 @@ namespace OpenEngine {
 	{
 		friend class EventDispatcher;
 	public:
+		virtual ~Event() = default;
+
 		bool Handled = false;
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
