c210f4c12b56fb5381c2aeadf48aaa4c5329fbec
ib0nk1b0 <iamk1720@gmail.com>
2023-07-13 16:12:12 +0100

    Updated premake files

---
+++ Dependencies.lua
@@ -0,0 +1,19 @@
+
+VULKAN_SDK = os.getenv("VULKAN_SDK")
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/OpenEngine/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/OpenEngine/vendor/Glad/include"
+IncludeDir["ImGui"] = "%{wks.location}/OpenEngine/vendor/imgui"
+IncludeDir["glm"] = "%{wks.location}/OpenEngine/vendor/glm"
+IncludeDir["stb_image"] = "%{wks.location}/OpenEngine/vendor/stb_image"
+IncludeDir["entt"] = "%{wks.location}/OpenEngine/vendor/EnTT/include"
+IncludeDir["json"] = "%{wks.location}/OpenEngine/vendor/Json/"
+IncludeDir["ImGuizmo"] = "%{wks.location}/OpenEngine/vendor/ImGuizmo/"
+IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
+
+LibraryDir = {}
+LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
+
+Library = {}
+Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,7 @@
+include "Dependencies.lua"
+
 workspace "OpenEngine"
-  architecture "x64"
+  architecture "x86_64"
   startproject "OpenEngine-Editor"
 
   configurations
@@ -11,207 +13,20 @@ workspace "OpenEngine"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-IncludeDir = {}
-IncludeDir["GLFW"] = "OpenEngine/vendor/GLFW/include"
-IncludeDir["Glad"] = "OpenEngine/vendor/Glad/include"
-IncludeDir["ImGui"] = "OpenEngine/vendor/imgui"
-IncludeDir["glm"] = "OpenEngine/vendor/glm"
-IncludeDir["stb_image"] = "OpenEngine/vendor/stb_image"
-IncludeDir["entt"] = "OpenEngine/vendor/EnTT/include"
-IncludeDir["json"] = "OpenEngine/vendor/Json/"
-IncludeDir["ImGuizmo"] = "OpenEngine/vendor/ImGuizmo/"
-
 group "Dependencies"
   include "OpenEngine/vendor/GLFW"
   include "OpenEngine/vendor/Glad"
   include "OpenEngine/vendor/imgui"
 group ""
 
-project "OpenEngine"
-  location "OpenEngine"
-  kind "StaticLib"
-  language "C++"
-  cppdialect "C++17"
-  staticruntime "on"
+group "Core"
+  include "OpenEngine"
+group ""
 
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+group "Tools"
+include "OpenEngine-Editor"
+group ""
 
-  pchheader "oepch.h"
-  pchsource "OpenEngine/src/oepch.cpp"
-
-  files
-  {
-    "%{prj.name}/src/**.h",
-    "%{prj.name}/src/**.cpp",
-    "%{prj.name}/vendor/stb_image/**.h",
-    "%{prj.name}/vendor/stb_image/**.cpp",
-    "%{prj.name}/vendor/glm/glm/**.hpp",
-    "%{prj.name}/vendor/glm/glm/**.inl",
-    "%{prj.name}/vendor/ImGuizmo/ImGuizmo.h",
-    "%{prj.name}/vendor/ImGuizmo/ImGuizmo.cpp"
-  }
-
-  defines
-  {
-    "_CRT_SECURE_NO_WARNINGS"
-  }
-
-  includedirs
-  {
-    "%{prj.name}/src",
-    "%{prj.name}/vendor/spdlog/include",
-    "%{IncludeDir.GLFW}",
-    "%{IncludeDir.Glad}",
-    "%{IncludeDir.ImGui}",
-    "%{IncludeDir.glm}",
-    "%{IncludeDir.stb_image}",
-    "%{IncludeDir.entt}",
-    "%{IncludeDir.ImGuizmo}",
-    "%{IncludeDir.json}"
-  }
-
-  links
-  {
-    "GLFW",
-    "Glad",
-    "ImGui",
-    "opengl32.lib"
-  }
-
-  filter "files:OpenEngine/vendor/ImGuizmo/**.cpp"
-  flags { "NoPCH" }
-
-  filter "system:windows"
-    systemversion "latest"
-
-    defines
-    {
-    	"OE_PLATFORM_WINDOWS",
-    	"OE_BUILD_DLL",
-    	"GLFW_INCLUDE_NONE"
-    }
-
-  filter "configurations:Debug"
-    defines "OE_DEBUG"
-    runtime "Debug"
-    symbols "on"
-
-  filter "configurations:Release"
-    defines "OE_RELEASE"
-    runtime "Release"
-    optimize "on"
-
-  filter "configurations:Dist"
-    defines "OE_DIST"
-    runtime "Release"
-    optimize "on"
-
-project "Sandbox"
-  location "Sandbox"
-  kind "ConsoleApp"
-  language "C++"
-  cppdialect "C++17"
-  staticruntime "on"
-
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-  files
-  {
-    "%{prj.name}/src/**.h",
-    "%{prj.name}/src/**.cpp",
-    "%{prj.name}/vendor/glm/glm/**.hpp",
-    "%{prj.name}/vendor/glm/glm/**.inl"
-  }
-
-  includedirs
-  {
-    "OpenEngine/vendor/spdlog/include",
-    "OpenEngine/src",
-    "OpenEngine/vendor",
-    "%{IncludeDir.glm}",
-    "%{IncludeDir.entt}"
-  }
-
-  links
-  {
-    "OpenEngine"
-  }
-
-  filter "system:windows"
-    systemversion "latest"
-
-    defines
-    {
-      "OE_PLATFORM_WINDOWS"
-    }
-
-  filter "configurations:Debug"
-    defines "OE_DEBUG"
-    runtime "Debug"
-    symbols "on"
-
-  filter "configurations:Release"
-    defines "OE_RELEASE"
-    runtime "Release"
-    optimize "on"
-
-  filter "configurations:Dist"
-    defines "OE_DIST"
-    runtime "Release"
-    optimize "on"
-
-project "OpenEngine-Editor"
-  location "OpenEngine-Editor"
-  kind "ConsoleApp"
-  language "C++"
-  cppdialect "C++17"
-  staticruntime "on"
-
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-  files
-  {
-    "%{prj.name}/src/**.h",
-    "%{prj.name}/src/**.cpp"
-  }
-
-  includedirs
-  {
-    "OpenEngine/vendor/spdlog/include",
-    "OpenEngine/src",
-    "OpenEngine/vendor",
-    "%{IncludeDir.glm}",
-    "%{IncludeDir.entt}",
-    "%{IncludeDir.json}"
-  }
-
-  links
-  {
-    "OpenEngine"
-  }
-
-  filter "system:windows"
-    systemversion "latest"
-
-    defines
-    {
-      "OE_PLATFORM_WINDOWS"
-    }
-
-  filter "configurations:Debug"
-    defines "OE_DEBUG"
-    runtime "Debug"
-    symbols "on"
-
-  filter "configurations:Release"
-    defines "OE_RELEASE"
-    runtime "Release"
-    optimize "on"
-
-  filter "configurations:Dist"
-    defines "OE_DIST"
-    runtime "Release"
-    optimize "on"
+group "Projects"
+include "Sandbox"
+group ""
